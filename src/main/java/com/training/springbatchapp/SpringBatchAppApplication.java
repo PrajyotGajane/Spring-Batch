28faa85f575ecfb7dd51300fe127bae39d3735f3
PrajyotGajane <prajyotgajane@gmail.com>
2022-09-27 22:12:50 +0530

    First commit

--- src/main/java/com/training/springbatchapp/SpringBatchAppApplication.java
+++ src/main/java/com/training/springbatchapp/SpringBatchAppApplication.java
@@ -8,6 +8,7 @@ public class SpringBatchAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchAppApplication.class, args);
+	System.out.println("");
     }
 
 }
